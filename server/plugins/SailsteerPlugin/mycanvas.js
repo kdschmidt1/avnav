85e60df2e3b3d87fe51393161aac678099daeda0
pi <pi@pi-virtual-machine>
2021-08-19 23:56:31 +0200

    MapLayline.WP.xx.P2 solved
    seems everything to be ok!

--- server/plugins/SailsteerPlugin/mycanvas.js
+++ server/plugins/SailsteerPlugin/mycanvas.js
@@ -173,79 +173,65 @@ calc_LaylineAreas = function(props) {
 			WP_pos = new LatLon(props.WPposition.lat, props.WPposition.lon);
 
 			// Schnittpunkte berechnen
-			var is_SB = LatLon.intersection(b_pos, props.LLSB, WP_pos, props.LLBB + 180)
-					var is_BB = LatLon.intersection(b_pos, props.LLBB, WP_pos, props.LLSB + 180)
-					/*
-					* @param   {Number} brng: Initial bearing in degrees
-					* @param   {Number} dist: Distance in km
-					* @returns {LatLon} Destination point
-
-					LatLon.prototype.destinationPoint = function(brng, dist) {
-						*/
-						calc_endpoint = function(intersection, pos) {
-							let is_xx;
-							let dist_xx = pos.rhumbDistanceTo(intersection);	// in km
-							if(dist_xx > props.sailsteerlength/1000) // wenn abstand gösser gewünschte LL-Länge, neuen endpunkt der LL berechnen
-							is_xx = b_pos.rhumbDestinationPoint(b_pos.rhumbBearingTo(intersection), props.sailsteerlength/1000)
-									else if(dist_xx< props.sailsteerlength/1000 && props.sailsteeroverlap)// wenn abstand kleiner gewünschte LL-Länge und Verlängerung über schnittpunkt gewollt, neuen endpunkt der LL berechnen
-							is_xx = b_pos.rhumbDestinationPoint(b_pos.rhumbBearingTo(intersection), props.sailsteerlength/1000)
-									else	// sonst: endpunkt der der LL ist Schnittpunkt
-							is_xx= intersection;
-							return(is_xx)
-						};
+			var is_SB = LatLon.intersection(b_pos, props.LLSB, WP_pos, props.LLBB + 180);
+			var is_BB = LatLon.intersection(b_pos, props.LLBB, WP_pos, props.LLSB + 180);
+			calc_endpoint = function(intersection, pos) {
+				let is_xx;
+				let dist_xx = pos.rhumbDistanceTo(intersection);	// in km
+				if(dist_xx > props.sailsteerlength/1000) // wenn abstand gösser gewünschte LL-Länge, neuen endpunkt der LL berechnen
+					is_xx = pos.rhumbDestinationPoint(pos.rhumbBearingTo(intersection), props.sailsteerlength/1000)
+				else if(dist_xx< props.sailsteerlength/1000 && props.sailsteeroverlap)// wenn abstand kleiner gewünschte LL-Länge und Verlängerung über schnittpunkt gewollt, neuen endpunkt der LL berechnen
+					is_xx = pos.rhumbDestinationPoint(pos.rhumbBearingTo(intersection), props.sailsteerlength/1000)
+				else
+					is_xx= intersection;
+				return(is_xx)
+			};
 
 
-						if(is_BB)
-						{
-							is_BB_boat=calc_endpoint(is_BB, b_pos);
-							is_BB_WP = calc_endpoint(is_BB, WP_pos);
-						}
+			if(is_BB)
+			{
+				is_BB_boat=calc_endpoint(is_BB, b_pos);
+				is_BB_WP = calc_endpoint(is_BB, WP_pos);
+			}
 
-						if(is_SB)
-						{
-							is_SB_boat=calc_endpoint(is_SB, b_pos);
-							is_SB_WP = calc_endpoint(is_SB, WP_pos);
-						}
-							var dist_SB = b_pos.distanceTo(is_SB);
+			if(is_SB)
+			{
+				is_SB_boat=calc_endpoint(is_SB, b_pos);
+				is_SB_WP = calc_endpoint(is_SB, WP_pos);
+			}
 
-						if(is_SB && is_BB){	// es gibt schnittpunkte
-							this.ep_BB_boat = is_BB_boat;
-							this.ep_BB_WP = is_BB_WP;
-							this.ep_SB_boat = is_SB_boat;
-							this.ep_SB_WP = is_SB_WP;
-							//MapLayline.WP.xx.P2 ist verkehrt!!
-							this.MapLayline = { Boat: { SB: { P1: b_pos, P2: is_SB_boat, color: 'rgb(0,255,0)' }, BB: { P1: b_pos, P2: is_BB_boat, color: 'red' } }, WP: { SB: { P1: WP_pos, P2: is_SB_WP, color: 'red' }, BB: { P1: WP_pos, P2: is_BB_WP, color: 'rgb(0,255,0)' } } }
-						}
-						else
-							this.MapLayline = null; // wenn abstand zu gross, endpunkte der LL berechnen
-					}
+			if(is_SB && is_BB){	
+				// es gibt schnittpunkte
+				this.MapLayline = 
+				{ 
+				  Boat: { SB: { P1: b_pos, P2: is_SB_boat, color: 'rgb(0,255,0)' }, 
+				          BB: { P1: b_pos, P2: is_BB_boat, color: 'red' } }, 
+				  WP:   { SB: { P1: WP_pos, P2: is_SB_WP, color: 'red' }, 
+		                  BB: { P1: WP_pos, P2: is_BB_WP, color: 'rgb(0,255,0)' } } 
+				}
+			}
+			else
+				this.MapLayline = null; // keine schnittpunkte
+			}
 		}
 		catch (e) {
 			// Anweisungen für jeden Fehler
 			console.log(e); // Fehler-Objekt an die Error-Funktion geben
 		}
 
-
-
-
-
 		// Berechnungen für die Laylineareas
-		// Die Breite der Areas (Winkelbereich) wird über die Refreshzeit abgebaut
-
-
+		// Die Breite der Areas (Winkelbereiche) wird über die Refreshzeit abgebaut
 		let reduktionszeit = props.sailsteerrefresh * 60
 
 		let difftime = (performance.now() - old_time) / 1000 // sec
 		old_time = performance.now()
 
-									let k = ln0_1 / reduktionszeit
+		let k = ln0_1 / reduktionszeit
 		for (var i = 0; i < 2; i++)
 			TWD_Abweichung[i] *= Math.exp(k * difftime)
 
 
-			let winkelabweichung = 0;
-		winkelabweichung = props.TWD - props.TSS
-		winkelabweichung %= 360
+		let winkelabweichung = (props.TWD - props.TSS) % 360;
 		if (Math.abs(winkelabweichung) > 180)
 			winkelabweichung = winkelabweichung < -180 ? winkelabweichung % 180 + 180 : winkelabweichung
 		winkelabweichung = winkelabweichung > 180 ? winkelabweichung % 180 - 180 : winkelabweichung
